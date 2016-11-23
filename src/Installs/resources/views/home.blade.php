6f7c1077641e447b893e3298aede9cf545dfdb45
Valeriy <adoafw@gmail.com>
2016-11-22 22:44:14 -0800

    Update home.blade.php

--- src/Installs/resources/views/home.blade.php
+++ src/Installs/resources/views/home.blade.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>{{ LAConfigs::getByKey('sitename') }}</title>	
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="{{ LAConfigs::getByKey('site_description') }}">
-    <meta name="author" content="Dwij IT Solutions">
-
+    <meta name="author" content="{{ LAConfigs::getByKey('site_maker') }}">
+    
     <meta property="og:title" content="{{ LAConfigs::getByKey('sitename') }}" />
     <meta property="og:type" content="website" />
     <meta property="og:description" content="{{ LAConfigs::getByKey('site_description') }}" />
     
-    <meta property="og:url" content="http://laraadmin.com/" />
-    <meta property="og:sitename" content="laraAdmin" />
-	<meta property="og:image" content="http://demo.adminlte.acacha.org/img/LaraAdmin-600x600.jpg" />
+    <meta property="og:url" content="{{ LAConfigs::getByKey('site_link') }}" />
+    <meta property="og:image" content="{{ LAConfigs::getByKey('site_image') }}" />
     
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:site" content="@laraadmin" />
     <meta name="twitter:creator" content="@laraadmin" />
     
-    <title>{{ LAConfigs::getByKey('sitename') }}</title>
     
     <!-- Bootstrap core CSS -->
     <link href="{{ asset('/la-assets/css/bootstrap.css') }}" rel="stylesheet">
