e2f2fdab74c8d2d989ba3780fc1479b4ad69e09d
Valeriy <adoafw@gmail.com>
2017-04-04 15:52:42 -0700

    Update routes.php

--- src/Installs/app/routes.php
+++ src/Installs/app/routes.php
@@ -2,3 +2,7 @@
 /* ================== Homepage + Admin Routes ================== */
 
 require __DIR__.'/admin_routes.php';
+
+/* ================== Feedback Route ================== */
+
+Route::post('/', ['as'=>'contactus.store','uses'=>'ContactUSController@contactUSPost']);
