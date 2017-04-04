3ce1f0c8c33a87214cb347722dd598cb1ecb7b1c
Valeriy <adoafw@gmail.com>
2017-04-04 15:45:23 -0700

    Update footer.blade.php

--- src/Installs/resources/views/la/layouts/partials/footer.blade.php
+++ src/Installs/resources/views/la/layouts/partials/footer.blade.php
@@ -1,8 +1,11 @@
 @if(!isset($no_padding))
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        Powered by <a href="http://dwijitsolutions.com">Dwij IT Solutions</a>
+        Powered by <a href="http:/pixelsforge.com">PixelsForge</a>
     </div>
-    <strong>Copyright &copy; 2016
+    <div class="pull-right hidden-xs">
+        Admin panel by <a href="http://dwijitsolutions.com">Dwij IT Solutions</a>
+    </div>
+    <strong>Copyright &copy; 2017
 </footer>
 @endif
